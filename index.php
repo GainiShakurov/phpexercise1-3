f98b9eeaa01d1288bf43f193ea854e8553e690a4
Гайни <jokker1987@mail.ru>
2018-05-17 09:12:25 +0600

    Исправления

--- index.php
+++ index.php
@@ -14,42 +14,38 @@ echo '<pre>';
 print_r($animals);
 echo '</pre>';
 
-$wordsCounter = 0;
+$firstHalf = $secondHalf = $doubles = [];
 
 foreach ($animals as $continent => $value_array) {
     foreach ($value_array as $value_animal) {
         if (str_word_count($value_animal) === 2) {
             $doubles[$continent][] = $value_animal;
-            $wordsCounter++;
-            $firstHalf[$continent][] = explode(' ', $value_animal)[0];
-            $secondHalf[] = explode(' ', $value_animal)[1];
+            $exploded = explode(' ', $value_animal);
+            $firstHalf[$continent][] = $exploded[0];
+            $secondHalf[] = $exploded[1];
         }
     }
 }
+
+shuffle($secondHalf);
 
 echo '<h3>Животные из двух слов</h3>';
 echo '<pre>';
 print_r($doubles);
 echo '</pre>';
 
-$arrayKeys = '';
-
-for ($i = 0; $i < $wordsCounter; $i++) {
-    $arrayKeys.= strval($i);
-}
-
-$randomNumbers = str_shuffle($arrayKeys);
+$counter = 0;
 
 foreach ($firstHalf as $continent => $animals) {
     foreach ($animals as $key => $animal) {
-        $firstHalf[$continent][$key].= ' '.$secondHalf[substr($randomNumbers, 0, 1)];
-        $randomNumbers = substr($randomNumbers, 1);
+        $firstHalf[$continent][$key] .= ' ' . $secondHalf[$counter];
+        $counter++;
     }
 }
 
 echo '<h3>Итоговый результат</h3>';
 
 foreach ($firstHalf as $continent => $animals) {
-    echo '<h2>'.$continent.'</h2>';
+    echo '<h2>' . $continent . '</h2>';
     echo implode(', ', $animals);
 }
